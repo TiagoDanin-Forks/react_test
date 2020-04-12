d402083a8eea05866a4d64244c10d230186a92bf
Waldener Monteiro <jjrr019@gmail.com>
2020-04-11 22:04:05 -0300

    Updates

---
+++ precache-manifest.1f92c8448207eff146bcf97c108c2e60.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "30676c7170d5ce223a4a0236aabecff4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c13cf1e965e26263cdee",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "cb43f9ffaa03ec2cbcfe",
+    "url": "/static/js/2.8785d699.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.8785d699.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c13cf1e965e26263cdee",
+    "url": "/static/js/main.2c6fc929.chunk.js"
+  },
+  {
+    "revision": "1b6c384f9157cead42ec",
+    "url": "/static/js/runtime-main.8a2a75dc.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/static/media/logo.5d5d9eef.svg"
+  }
+]);
